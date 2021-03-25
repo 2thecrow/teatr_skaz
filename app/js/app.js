9135b7c19724c9795744b98b64731991a0e7cd24
Alexander Pletnev <pletnev.a.a@yandex.ru>
2021-03-25 21:07:34 +0200

    Додела главную

--- app/js/app.js
+++ app/js/app.js
@@ -2,22 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	const swiper = new Swiper('.welcome-slider-container', {
 		// Optional parameters
-		loop: true,
-	  
-		// If we need pagination
-		pagination: {
-		  el: '.swiper-pagination',
-		},
+		/* loop: true, */
 	  
 		// Navigation arrows
 		navigation: {
 		  nextEl: '.swiper-button-next',
 		  prevEl: '.swiper-button-prev',
-		},
-	  
-		// And if we need scrollbar
-		scrollbar: {
-		  el: '.swiper-scrollbar',
 		},
 	});
 	
@@ -27,20 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
 		slidesPerView: 5,
 		spaceBetween: 15,
 	  
-		// If we need pagination
-		pagination: {
-		  el: '.swiper-pagination',
-		},
-	  
 		// Navigation arrows
 		navigation: {
 		  nextEl: '.swiper-button-next',
 		  prevEl: '.swiper-button-prev',
-		},
-	  
-		// And if we need scrollbar
-		scrollbar: {
-		  el: '.swiper-scrollbar',
 		},
 	});
 	
